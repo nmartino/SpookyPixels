045447ab6a56131649c669c959a9e0a2b452e003
nmartino <nikolas.martino@gmail.com>
2024-11-12 19:16:27 -0300

    arreglado error con treasure

--- relics/necronomicon.gd
+++ relics/necronomicon.gd
@@ -5,7 +5,7 @@ var already_initialized :=  false
 func initialize_relic(owner: RelicUI) -> void:
 	if already_initialized:
 		return
-	var run := owner.get_tree().get_first_node_in_group("run") as Run
+	var run := owner.get_tree().get_first_node_in_group("run")
 	run.character.max_mana += 1
 	run.character.mana = run.character.max_mana
 	already_initialized = true
@@ -16,6 +16,6 @@ func activate_relic(owner: RelicUI) -> void:
 	owner.flash()
 	
 func deactivate_relic(owner: RelicUI) -> void:
-	var run := owner.get_tree().get_first_node_in_group("run") as Run
+	var run := owner.get_tree().get_first_node_in_group("run")
 	run.character.max_mana -= 1
 	
