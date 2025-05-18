a8dce81f22662ea8a86bd9f3852d59944ec753f1
Fran Ayrolo <fran.ayrolo@gmail.com>
2025-05-18 14:06:50 -0300

    Barra espaciadora para saltar intros

--- Scenes/splash/splash_screen.gd
+++ Scenes/splash/splash_screen.gd
@@ -7,22 +7,32 @@ const MAIN_MENU_PATH := "res://Scenes/UI/main_menu.tscn"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var music: AudioStream
 
+var animations: Array[StringName] = ["logos", "logoPixel"]
+var skip_spamming_locked: bool = false
+
 func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
 	MusicPlayer.play(music, true)
 	
-	
-	
+	animation_player.stop()
+	animation_player.clear_queue()
+	for a in animations:
+		animation_player.queue(a)
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("spacebar") && !skip_spamming_locked:
+		skip_spamming_locked = true
+		animation_player.seek(animation_player.current_animation_length - 0.01)
+		get_tree().create_timer(0.5).timeout.connect(
+			func(): skip_spamming_locked = false
+		)
 
 func _play_swoosh() -> void:
 	SFXPlayer.play(swoosh)
 	
-
 func _play_appear() -> void:
 	SFXPlayer.play(ghost_appear)
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "logos":
-		animation_player.play("logoPixel")
-	
 	if anim_name == "logoPixel":
 		get_tree().change_scene_to_file(MAIN_MENU_PATH)
